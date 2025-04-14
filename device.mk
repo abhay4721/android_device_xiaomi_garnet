14bfdeefee2e516e1f164567347e7dfba9b9c2b5
MySellySenpai <xusufgul@gmail.com>
2025-04-14 19:59:25 +0000

    Revert "garnet: Move DAX default to device tree and add FourSemi config"
    This reverts commit c2fabbce03f476c3af31d6b6554acf6d36865f99.

--- device.mk
+++ device.mk
@@ -115,10 +115,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml \
-    $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-fs19xx.xml
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.bluetooth.default:64 \
