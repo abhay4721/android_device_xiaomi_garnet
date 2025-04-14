127b227fd37169e98f0e7812101fb2a840d4ea30
MySellySenpai <xusufgul@gmail.com>
2025-04-14 20:01:25 +0000

    Revert "[WIP] garnet: rootdir: Remove /data/vendor/dolby on upgrade"
    This reverts commit d56695378b88e2cda7730e55a1f6bfc3f6aa145a.
    
    	modified:   device.mk
    	deleted:    rootdir/bin/init.dolby_fix.sh
    	modified:   rootdir/etc/init.qcom.rc
    	modified:   sepolicy/vendor/file_contexts
    	deleted:    sepolicy/vendor/init-qcom-dolby-sh.te

--- device.mk
+++ device.mk
@@ -273,8 +273,7 @@ PRODUCT_PACKAGES += \
     ueventd.qcom.rc:64
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom \
-    $(LOCAL_PATH)/rootdir/bin/init.dolby_fix.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.dolby_fix.sh
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
 
 # IPACM
 PRODUCT_PACKAGES += \
