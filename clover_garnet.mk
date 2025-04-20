0bebb3a0c141ef4214f988345461faf0f83abb0b
Abhayabhi4721 <abhayabhi4721@gmail.com>
2025-04-20 19:28:26 +0530

    Shello cloverproject

--- clover_garnet.mk
+++ clover_garnet.mk
@@ -11,12 +11,25 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 TARGET_SUPPORTS_OMX_SERVICE := false
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/clover/config/common_full_phone.mk)
 
 # Inherit from garnet device
 $(call inherit-product, device/xiaomi/garnet/device.mk)
 
-PRODUCT_NAME := lineage_garnet
+# Device config
+TARGET_ENABLE_BLUR := true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# GAPPS (valid only for GAPPS builds)
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_MATLOG := true
+
+PRODUCT_NAME := clover_garnet
 PRODUCT_DEVICE := garnet
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
@@ -25,6 +38,12 @@ PRODUCT_MODEL := 2312DRA50G
 PRODUCT_SYSTEM_NAME := garnet_global
 PRODUCT_SYSTEM_DEVICE := garnet
 
+# Clover assortment
+ TARGET_FACE_UNLOCK_SUPPORTED := true
+ TARGET_SUPPORTS_GOOGLE_RECORDER := true
+ TARGET_SUPPORTS_QUICK_TAP := true
+ USE_PIXEL_CHARGER := true 
+ 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="garnet_global-user 15 AQ3A.240912.001 OS2.0.10.0.VNRMIXM release-keys" \
     BuildFingerprint=POCO/garnet_global/garnet:15/AQ3A.240912.001/OS2.0.10.0.VNRMIXM:user/release-keys \
